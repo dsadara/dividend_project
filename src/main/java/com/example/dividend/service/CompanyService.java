f72afc1470050f102cba49f963ba16877225bc42
LIM <lim971230@gmail.com>
2023-01-27 13:17:30 +0900

    예외 처리
    1. 커스텀 익셉션 구현
    AbstractException을 구현한 NoCompanyException, AlreadyExistUserException
    2. ExceptionHandler 사용해서 CustomeException 발생 시 에러코드, 메시지 클라이언트에 응답하기

--- src/main/java/com/example/dividend/service/CompanyService.java
+++ src/main/java/com/example/dividend/service/CompanyService.java
@@ -1,5 +1,6 @@
 package com.example.dividend.service;
 
+import com.example.dividend.exception.impl.NoCompanyException;
 import com.example.dividend.model.Company;
 import com.example.dividend.model.ScrapedResult;
 import com.example.dividend.persist.CompanyRepository;
@@ -84,7 +85,7 @@ public class CompanyService {
 
     public String deleteCompany(String ticker) {
         var company = this.companyRepository.findByTicker(ticker)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 회사입니다."));
+                .orElseThrow(() -> new NoCompanyException());
 
         // 배당금 삭제 후 회사도 삭제
         this.dividendRepository.deleteAllByCompanyId(company.getId());
